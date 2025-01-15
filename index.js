dacc5899b418ddb6cebe5f97aa68e3c3434412ea
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-15 14:59:30 -0800

    intial commit to remove code

---
+++ index.js
@@ -0,0 +1,31 @@
+// initialize words array - hold all words we enter 
+const words = []
+
+
+
+
+
+/**
+ * Update the output to reflect `words`
+ */
+
+// const render = () => {}
+function render() {
+// TODO
+}
+
+
+/**
+ * If the user clicks on a word, remove it from the sentence.
+ * @param {Event} event the deepest element that was clicked
+ */
+function onWordClick(event) {
+// TODO
+}
+
+/**
+ * Add the submitted word into `words`, clear the form, and rerender.
+ */
+function addWord(event) {
+// TODO
+}
